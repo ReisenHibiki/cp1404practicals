548e2e799b3021aadba618c8bbb0fdc3b4b5134c
Hengyu Jiang <hengyu.jiang@my.jcu.edu.au>
2022-11-18 18:06:50 +0800

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,19 @@
-password = input("Enter password: ")
-while len(password) < 6:
-    print("Password is too short!")
-    password = input("Enter password: ")
-print("*" * len(password))
+def main():
+    """"""
+    password = get_password()
+    display_asterisks(password)
 
+
+def display_asterisks(password):
+    print("*" * len(password))
+
+
+def get_password():
+    password = input("Enter password: ")
+    while len(password) < 6:
+        print("Password is too short!")
+        password = input("Enter password: ")
+    return password
+
+
+main()
