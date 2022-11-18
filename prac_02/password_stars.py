e6100a00c02564241162ab6da1376a3a5523d83e
Hengyu Jiang <hengyu.jiang@my.jcu.edu.au>
2022-11-18 18:15:21 +0800

    Add docstring

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,14 +1,16 @@
 def main():
-    """"""
+    """get password and display asterisks"""
     password = get_password()
     display_asterisks(password)
 
 
 def display_asterisks(password):
+    """display asterisks"""
     print("*" * len(password))
 
 
 def get_password():
+    """get valid password"""
     password = input("Enter password: ")
     while len(password) < 6:
         print("Password is too short!")
