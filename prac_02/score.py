6ef6a16537dbd4585be5f61c4fa22be52afec156
Hengyu Jiang <hengyu.jiang@my.jcu.edu.au>
2022-11-18 18:37:36 +0800

    refactor determine grade into function and add random score

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,29 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+
+def main():
+    """ask the user for their score and print the result."""
+    score = float(input("Enter score: "))
+    result = determine_result(score)
+    print(result)
+    random_score = random.randint(0,100)
+    result = determine_result(random_score)
+    print(result)
+
+
+def determine_result(score):
+    """determine score"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
