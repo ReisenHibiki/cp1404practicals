f0d0713e0782f77d413b4e93a2106f59a63bc6f1
Hengyu Jiang <hengyu.jiang@my.jcu.edu.au>
2022-11-25 17:14:02 +0800

    Answer the questions and write a code to print a random number between 1 and 100 inclusive

---
+++ prac_03/randoms.py
@@ -0,0 +1,16 @@
+import random
+
+# What did you see on line 1?
+# I saw 12, 16, 8, 15, 9, 18 and 19
+# The smallest number I could see is 5 and the largest number is 20.
+
+# What did you see on line 2?
+# I saw 3, 5, 7 and 9
+# the smallest number I could see is 3 and the largest one is 9
+# no, line2 code could not produce 4
+
+# What did you see on line 3?
+# I saw 4.953749134321164, 5.456749863418499 and 5.029736589451598
+# The smallest number is 2.5 and the largest number is 5.5
+
+print(random.randint(1, 100))
