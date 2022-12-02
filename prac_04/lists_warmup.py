893e437a2fddc33fec5f297015d88a50ad7aa2d2
Hengyu Jiang <hengyu.jiang@my.jcu.edu.au>
2022-12-02 16:49:11 +0800

    initial commit and answer the question and statement

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,26 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# Answers:
+# 3
+# 2
+# 1
+# [3, 1, 4, 1, 5, 9]
+# [1]
+# True
+# False
+# False
+# [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# Change the first element of numbers to "ten" (the string, not the number 10)
+numbers[0] = "ten"
+print(numbers)
+
+# Change the last element of numbers to 1
+numbers[-1] = 1
+print(numbers)
+
+# Print all the elements from numbers except the first two (slice)
+print(numbers[2:])
+
+# Print whether 9 is an element of numbers
+print(9 in numbers)
