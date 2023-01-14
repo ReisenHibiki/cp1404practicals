26b3929bf7086c3cd2b7335b4e15962665a1b4a1
Hengyu Jiang <hengyu.jiang@my.jcu.edu.au>
2023-01-14 17:55:46 +0800

    Complete wiki prac

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -2,10 +2,12 @@ import wikipedia
 
 user_input = input(">>> ")
 while user_input != "":
-    page = wikipedia.page(user_input, auto_suggest=False)
-    print(page.title)
-    print(page.summary)
-    print(page.url)
-
+    try:
+        page = wikipedia.page(user_input, auto_suggest=False)
+        print(page.title)
+        print(page.summary)
+        print(page.url)
+    except wikipedia.WikipediaException:
+        print("Error, please try again.")
     user_input = input(">>> ")
 
